bee69a1514aaa9f5d9ed8f6e538f60ff30820ac3
PabloMednozaCoste <139722839+PabloMednozaCoste@users.noreply.github.com>
2023-07-29 02:57:49 -0400

    configuración básica de servidor

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+const routes = require('./routes');
+
+// Middleware para parsear el cuerpo de las solicitudes en formato JSON
+app.use(express.json());
+
+// Middleware para agregar las rutas
+app.use(routes);
+
+// Iniciar el servidor y escuchar en el puerto 3000
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
