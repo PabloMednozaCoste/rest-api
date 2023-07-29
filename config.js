2da06c8d93ae6edb98703b380dabb7585ec9e27a
PabloMednozaCoste <139722839+PabloMednozaCoste@users.noreply.github.com>
2023-07-29 02:03:02 -0400

    Inicio desarrollo

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    db: {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'Store_db',
+    },
+    jwt: {
+      secret: 'your-secret-key',
+    },
+  };
